31e123c5984d30bc7562f4332c89a06ac3e4df3c
Saad Elkheety <saadelkheety@gmail.com>
2018-06-19 00:40:22 +0200

    second commit

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,27 +2,27 @@ import media
 import fresh_tomatoes
 # create an instance of Escpae plan 2 movie
 ESCAPE = media.Movie("Escape Plan 2: Hades",
-                     "https://m.media-amazon.com/images/M/MV5BMTk4NjA0MjUyMF5BMl5BanBnXkFtZTgwMTEzNDQ1NTM@._V1_SY1000_CR0,0,666,1000_AL_.jpg",  # a url for the poster image
+                     "https://m.media-amazon.com/images/M/MV5BMTk4NjA0MjUyMF5BMl5BanBnXkFtZTgwMTEzNDQ1NTM@._V1_SY1000_CR0,0,666,1000_AL_.jpg",  # noqa
                      "https://youtu.be/6Q7vVt-MeXk",
                      "2018 Action 1h 34m")
 # create an instance of Ant-Man movie
 ANT_MAN = media.Movie("Ant-Man and the Wasp",
-                      "https://m.media-amazon.com/images/M/MV5BYjcyYTk0N2YtMzc4ZC00Y2E0LWFkNDgtNjE1MzZmMGE1YjY1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SY1000_CR0,0,675,1000_AL_.jpg",  # a url for the poster image
+                      "https://m.media-amazon.com/images/M/MV5BYjcyYTk0N2YtMzc4ZC00Y2E0LWFkNDgtNjE1MzZmMGE1YjY1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SY1000_CR0,0,675,1000_AL_.jpg",  # noqa
                       "https://youtu.be/8_rTIAOohas",
                       "2018 Fantasy/Science fiction film 2h 5m")
 # create an instance of Avengers: Infinity War movie
 INFINITY_WAR = media.Movie("Avengers: Infinity War",
-                           "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SY1000_CR0,0,674,1000_AL_.jpg",  # a url for the poster image
+                           "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SY1000_CR0,0,674,1000_AL_.jpg",  # noqa
                            "https://youtu.be/6ZfuNTqbHE8",
                            "2018 Fantasy/Science fiction film 2h 40m")
 
 DEADPOOL_2 = media.Movie("Deadpool 2",
-                         "https://m.media-amazon.com/images/M/MV5BMjI3Njg3MzAxNF5BMl5BanBnXkFtZTgwNjI2OTY0NTM@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+                         "https://m.media-amazon.com/images/M/MV5BMjI3Njg3MzAxNF5BMl5BanBnXkFtZTgwNjI2OTY0NTM@._V1_SY1000_CR0,0,674,1000_AL_.jpg",  # noqa
                          "https://youtu.be/wLeGWcVeIZ4",
                          " 2018 Fantasy/Science fiction film 2 hours")
 
 THOR_R = media.Movie("Thor: Ragnarok",
-          "https://m.media-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+          "https://m.media-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_SY1000_CR0,0,674,1000_AL_.jpg",  # noqa
           "https://youtu.be/ue80QwXMRHg",
           "2017 Fantasy/Science fiction film 2h 10m")
 # create the list to pass it to the open_movies_page method
--- media.py
+++ media.py
@@ -1,9 +1,18 @@
 from webbrowser import open_new_tab
+
+
 class Movie():
-    def __init__(self, movie_title, poster_image, trailer_youtube,box_art):
+    """ this class is instaniate a movie thumbnail"""
+
+    def __init__(self, movie_title, poster_image, trailer_youtube, box_art):
+        """ this is the constructor of the class to provide the required
+        information of the movie whch is the title, the poster image link,
+        the youtube trailer link, and the box art of the movie"""
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
         self.box_art = box_art
+
     def show_trailer(self):
+        """ this method is to show up the trailer in a new tab"""
         open_new_tab(self.trailer_youtube_url)
